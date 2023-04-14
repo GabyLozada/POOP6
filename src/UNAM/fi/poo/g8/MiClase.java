bfb44a90babc3148e858e4996d2ac44c71fc45eb
GabyLozada <78162695+GabyLozada@users.noreply.github.com>
2023-04-14 01:19:30 -0600

    Creación de practica

---
+++ src/UNAM/fi/poo/g8/MiClase.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UNAM.fi.poo.g8;
+
+/**
+ *
+ * @author Gabriela Aquino Lozada 
+ * @author Renata Barbosa Reyes
+ * @author Ruben Alberto Ortega Macias
+ */
+public class MiClase {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        
+    }
+    /**
+     * Metodo que realiza la multiplicacion de un entero por un flotante 
+     * y retorna un double
+     * @param a Parametro de tipo entero
+     * @param b Parametro de tipo flotante
+     * @return un valor de tipo double
+     */
+    public static double metodo(int a, float b){
+        return a*b;
+    }
+}
